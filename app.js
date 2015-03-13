8b9c66fc305e5c8d725b299f77271ccd299e243c
Charles Roper <charles@roper.im>
2015-03-13 18:24:08 +0000

    Back to spaces. Didn't like tabs.

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 var app = angular.module('gemStore', []);
 
 var gem = {
-	name: 'Azurite',
-	price: 110.50,
-	canPurchase: false,
-	soldOut: true
+  name: 'Azurite',
+  price: 110.50,
+  canPurchase: false,
+  soldOut: true
 };
 
 app.controller('StoreController', function () {
-	this.product = gem;
+  this.product = gem;
 });
