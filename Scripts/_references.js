047373abd5c95e0722f24f048a7ea352bee08681
Charles Roper <charles@roper.im>
2015-04-16 08:44:06 +0100

    Fix up the _references.js file
    So that it uses non-min Angular file.

--- Scripts/_references.js
+++ Scripts/_references.js
@@ -1,6 +1,6 @@
 ﻿/// <autosync enabled="true" />
 /// <reference path="../app.js" />
-/// <reference path="../bower_components/angular/angular.min.js" />
+/// <reference path="../bower_components/angular/angular.js" />
 /// <reference path="../bower_components/angular-bootstrap/ui-bootstrap-tpls.min.js" />
 /// <reference path="../bower_components/angular-bootstrap/ui-bootstrap.min.js" />
 /// <reference path="../bower_components/angular/index.js" />
