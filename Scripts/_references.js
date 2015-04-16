ecb6c512e4ea87dc3e974c39f952c149cca07be7
Charles Roper <charles@roper.im>
2015-04-16 08:37:23 +0100

    Start using Bower

--- Scripts/_references.js
+++ Scripts/_references.js
@@ -1,3 +1,7 @@
 ﻿/// <autosync enabled="true" />
 /// <reference path="angular.js" />
 /// <reference path="../app.js" />
+/// <reference path="../bower_components/angular/angular.min.js" />
+/// <reference path="../bower_components/angular-bootstrap/ui-bootstrap-tpls.min.js" />
+/// <reference path="../bower_components/angular-bootstrap/ui-bootstrap.min.js" />
+/// <reference path="../bower_components/angular/index.js" />
